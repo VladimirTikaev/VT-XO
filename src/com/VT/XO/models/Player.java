16b7c036f73b59a9e9fb362152177a97141c6742
vladimirtr <vladimirt.215@gmail.com>
2019-10-22 13:37:24 +0300

    Create Player and BuilderPlayer as a inner class #2

---
+++ src/com/VT/XO/models/Player.java
@@ -0,0 +1,68 @@
+package com.VT.XO.models;
+
+
+public class Player {
+
+    private final String name;
+    private final String surname;
+    private final Figure figure;
+
+    private Player(PlayerBuilder playerBuilder){
+        this.name = playerBuilder.getName();
+        this.surname = playerBuilder.getSurname();
+        this.figure = playerBuilder.getFigure();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public class PlayerBuilder{
+
+        private String name;
+        private String surname;
+        private Figure figure;
+
+        public PlayerBuilder name(final String name){
+            this.name = name;
+            return this;
+        }
+
+        public PlayerBuilder surname(final String surname){
+            this.surname = surname;
+            return this;
+        }
+
+        public PlayerBuilder figure(final Figure figure){
+            this.figure = figure;
+            return this;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public Figure getFigure() {
+            return figure;
+        }
+
+        public Player build(){
+            return new Player(this);
+
+        }
+
+    }
+
+}
