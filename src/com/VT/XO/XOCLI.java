51d35c1ee28ebad6ff62279cdb2ad01c63f64750
vladimirtr <vladimirt.215@gmail.com>
2019-10-29 09:56:01 +0300

    Change XOCLI class. Now user can write field size.

--- src/com/VT/XO/XOCLI.java
+++ src/com/VT/XO/XOCLI.java
@@ -7,9 +7,12 @@ import com.VT.XO.models.Game;
 import com.VT.XO.models.Player;
 import com.VT.XO.views.ConsoleView;
 
+import java.util.Scanner;
+
 public class XOCLI{
 
     public static void main(String[] args) {
+
 
         final Player firstPlayer = new Player.
                 PlayerBuilder().
@@ -22,16 +25,19 @@ public class XOCLI{
                 PlayerBuilder().
                 name("Petr").
                 surname("Petrov").
-                figure(Figure.X).
+                figure(Figure.O).
                 build();
         final Player[] players = {firstPlayer, secondPlayer};
 
-        final Game game = new Game("XO", new Field(4), players);
-        final ConsoleView consoleView = new ConsoleView();
-        consoleView.showGame(game);
+        System.out.println("Input field size:");
+        Scanner sc = new Scanner(System.in);
+        final int fieldSize = sc.nextInt();
 
+        final Game game = new Game("XO", new Field(fieldSize), players);
+        final ConsoleView consoleView = new ConsoleView();
+        consoleView.show(game);
         while (consoleView.makeMove(game)){
-            consoleView.showField(game);
+            consoleView.show(game);
         }
 
     }
